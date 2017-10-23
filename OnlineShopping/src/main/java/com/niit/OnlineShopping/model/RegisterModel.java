cbc297489a0b28a8f4650d304845cec89af8322c
Vineet Mulam <mulamrimc@gmail.com>
2017-10-23 15:56:39 +0530

    This is my updated project

---
+++ OnlineShopping/src/main/java/com/niit/OnlineShopping/model/RegisterModel.java
@@ -0,0 +1,32 @@
+package com.niit.OnlineShopping.model;
+
+import java.io.Serializable;
+
+import com.niit.OnlineBackend.model.Address;
+import com.niit.OnlineBackend.model.User;
+
+
+public class RegisterModel implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	private User user;
+	private Address billing;
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Address getBilling() {
+		return billing;
+	}
+	public void setBilling(Address billing) {
+		this.billing = billing;
+	}
+		
+}
